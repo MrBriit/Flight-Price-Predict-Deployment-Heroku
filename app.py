1831978868779ec2c83f98fcbe3d7eb075668299
MrBriit <44134816+MrBriit@users.noreply.github.com>
2020-08-16 11:01:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def home():
 def predict():
     if request.method == "POST":
 
-        # Date_of_Journey
+        # Date 
         date_dep = request.form["Dep_Time"]
         Journey_day = int(pd.to_datetime(date_dep, format="%Y-%m-%dT%H:%M").day)
         Journey_month = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").month)
@@ -33,7 +33,7 @@ def predict():
         Dep_min = int(pd.to_datetime(date_dep, format ="%Y-%m-%dT%H:%M").minute)
         # print("Departure : ",Dep_hour, Dep_min)
 
-        # Arrival
+        # Arrival Time
         date_arr = request.form["Arrival_Time"]
         Arrival_hour = int(pd.to_datetime(date_arr, format ="%Y-%m-%dT%H:%M").hour)
         Arrival_min = int(pd.to_datetime(date_arr, format ="%Y-%m-%dT%H:%M").minute)
@@ -46,10 +46,9 @@ def predict():
 
         # Total Stops
         Total_stops = int(request.form["stops"])
-        # print(Total_stops)
+       
 
-        # Airline
-        # AIR ASIA = 0 (not in column)
+        
         airline=request.form['airline']
         if(airline=='Jet Airways'):
             Jet_Airways = 1
@@ -207,20 +206,7 @@ def predict():
             Vistara_Premium_economy = 0
             Trujet = 0
 
-        # print(Jet_Airways,
-        #     IndiGo,
-        #     Air_India,
-        #     Multiple_carriers,
-        #     SpiceJet,
-        #     Vistara,
-        #     GoAir,
-        #     Multiple_carriers_Premium_economy,
-        #     Jet_Airways_Business,
-        #     Vistara_Premium_economy,
-        #     Trujet)
-
-        # Source
-        # Banglore = 0 (not in column)
+        
         Source = request.form["Source"]
         if (Source == 'Delhi'):
             s_Delhi = 1
@@ -251,14 +237,7 @@ def predict():
             s_Kolkata = 0
             s_Mumbai = 0
             s_Chennai = 0
-
-        # print(s_Delhi,
-        #     s_Kolkata,
-        #     s_Mumbai,
-        #     s_Chennai)
-
-        # Destination
-        # Banglore = 0 (not in column)
+            
         Source = request.form["Destination"]
         if (Source == 'Cochin'):
             d_Cochin = 1
@@ -301,26 +280,6 @@ def predict():
             d_New_Delhi = 0
             d_Hyderabad = 0
             d_Kolkata = 0
-
-        # print(
-        #     d_Cochin,
-        #     d_Delhi,
-        #     d_New_Delhi,
-        #     d_Hyderabad,
-        #     d_Kolkata
-        # )
-        
-
-    #     ['Total_Stops', 'Journey_day', 'Journey_month', 'Dep_hour',
-    #    'Dep_min', 'Arrival_hour', 'Arrival_min', 'Duration_hours',
-    #    'Duration_mins', 'Airline_Air India', 'Airline_GoAir', 'Airline_IndiGo',
-    #    'Airline_Jet Airways', 'Airline_Jet Airways Business',
-    #    'Airline_Multiple carriers',
-    #    'Airline_Multiple carriers Premium economy', 'Airline_SpiceJet',
-    #    'Airline_Trujet', 'Airline_Vistara', 'Airline_Vistara Premium economy',
-    #    'Source_Chennai', 'Source_Delhi', 'Source_Kolkata', 'Source_Mumbai',
-    #    'Destination_Cochin', 'Destination_Delhi', 'Destination_Hyderabad',
-    #    'Destination_Kolkata', 'Destination_New Delhi']
         
         prediction=model.predict([[
             Total_stops,
